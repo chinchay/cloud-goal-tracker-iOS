a944bbc77f81718659328b30d826e6d8dede7e4b
Carlos Leon <35080984+chinchay@users.noreply.github.com>
2023-05-20 10:47:36 -0300

    feat: First working simple app with a button

---
+++ app/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  app
+//
+//  Created by Carlos Leon on 2023-05-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+            
+            Button("Click me"){
+                print("Hello world on the console!")
+            }
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ app/appApp.swift
@@ -0,0 +1,17 @@
+//
+//  appApp.swift
+//  app
+//
+//  Created by Carlos Leon on 2023-05-20.
+//
+
+import SwiftUI
+
+@main
+struct appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
